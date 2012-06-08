6bbaa342ccbb4393ad89f9beac5531a8eeed8c21
Cyrille Pontvieux <jrdATenialisDOTnet>
2012-06-08 23:45:01 +0000

    Preparing version 0.2.0 based on the upcoming SaLT version 0.2.0.

--- .getsalt.sh
+++ .getsalt.sh
@@ -3,8 +3,8 @@
 # Used to download the correct version of SaLT.
 
 cd "$(dirname "$0")"
-SALT_VER='0.1.1'
-SALT_URL="https://salix.svn.sourceforge.net/svnroot/salix/salt/tags/$SALT_VER"
+SALT_VER='trunk'
+SALT_URL="https://salix.svn.sourceforge.net/svnroot/salix/salt/$SALT_VER"
 if [ -d salt ]; then
   rm -rf salt || echo "salt directory cannot be removed, check permissions" >&2
 fi
