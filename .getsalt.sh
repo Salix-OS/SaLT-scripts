8d9a254ebb57677ab264213be00e77f9aef0e972
Cyrille Pontvieux <jrd@enialis.net>
2012-12-22 11:06:07 +0100

    Fix for glibc installation when /etc/localtime was missing. Fix kernel source directory to use the kernel version and not '2.*' which is wrong nowadays. New way of handling /dev/null for the last module. Safer. New module 'test' for testing the boot process of a Live CD without installing a full distro.

--- .getsalt.sh
+++ .getsalt.sh
@@ -3,7 +3,7 @@
 # Used to download the correct version of SaLT.
 
 cd "$(dirname "$0")"
-SALT_VER='0.2.0'
+SALT_VER='master'
 SALT_URL='git://github.com/jrd/SaLT.git'
 if [ -d salt ]; then
   rm -rf salt || echo "salt directory cannot be removed, check permissions" >&2
@@ -12,6 +12,6 @@ fi
 git clone -n "$SALT_URL" salt
 cd salt
 # Getting the commit referenced by the tag.
-git checkout tags/"$SALT_VER"
+git checkout "$SALT_VER"
 # Removing the git handling of the directory.
 rm -rf .git
