9e16732310389cfca74e824209ccfcbfbfd4aec0
Cyrille Pontvieux <jrd@enialis.net>
2014-02-26 18:28:14 +0100

    Version 0.2.2: upgraded for 14.1

--- liveenv/liveenv.sh
+++ liveenv/liveenv.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# vim: set syn=sh et ai sw=2 st=2 ts=2 tw=0:
-cd "$(dirname "$0")"
-[ "$(basename "$(pwd)")" = "liveenv" ] && cd ..
+# vim: set syn=sh ft=sh et sw=2 sts=2 ts=2 tw=0:
+cd $(dirname "$0")
+[ "$(basename $(pwd))" = liveenv ] && cd ..
 if [ -e MODULES.liveenv ]; then
   liveenv/create_txz.sh $(cat MODULES.liveenv)
 else
--- liveenv/modules/20-user-one/add.sh
+++ liveenv/modules/20-user-one/add.sh
@@ -11,7 +11,7 @@ modtxt="$HDIR"/MODIFICATIONS
 cat <<EOF >> "$modtxt"
 user-one
 -------------
-- user 'one' already created with no password
+- user 'one' already created with password 'one'
 - user 'one' added to the following groups:
   * floppy
   * audio
@@ -23,13 +23,14 @@ user-one
   * scanner
   * users
   * fuse
+  * wheel (so is sudoer)
 
 
 EOF
 
 echo 'one:x:1000:100::/home/one:/bin/bash' >> "$RDIR"/etc/passwd
 echo 'one::13686:0:99999:7:::' >> "$RDIR"/etc/shadow
-sed -i 's/^floppy:.*/\0,one/; s/^audio:.*/\0,one/; s/^video:.*/\0,one/; s/^cdrom:.*/\0,one/; s/^plugdev:.*/\0,one/; s/^power:.*/\0,one/; s/^netdev:.*/\0,one/; s/^scanner:.*/\0,one/; s/^users:.*/\0one/; s/^fuse:.*/\0one/;' "$RDIR"/etc/group
+sed -i 's/^floppy:.*/\0,one/; s/^audio:.*/\0,one/; s/^video:.*/\0,one/; s/^cdrom:.*/\0,one/; s/^plugdev:.*/\0,one/; s/^power:.*/\0,one/; s/^netdev:.*/\0,one/; s/^scanner:.*/\0,one/; s/^users:.*/\0one/; s/^fuse:.*/\0one/; s/^wheel:.*/\0one/;' "$RDIR"/etc/group
 mkdir -p "$RDIR"/home/one
 cp -r "$RDIR"/etc/skel/* "$RDIR"/etc/skel/.??* "$RDIR"/home/one/ 2>/dev/null
 
