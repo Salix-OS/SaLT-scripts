5714908aad2e30375945a7efeebe380f256cad7c
Dimitris Tzemos <djemos@salixos.org>
2014-03-20 12:38:17 +0200

    Update add.sh
    Correction needed for sed

--- liveenv/modules/20-user-one/add.sh
+++ liveenv/modules/20-user-one/add.sh
@@ -30,7 +30,7 @@ EOF
 
 echo 'one:x:1000:100::/home/one:/bin/bash' >> "$RDIR"/etc/passwd
 echo 'one::13686:0:99999:7:::' >> "$RDIR"/etc/shadow
-sed -i 's/^floppy:.*/\0,one/; s/^audio:.*/\0,one/; s/^video:.*/\0,one/; s/^cdrom:.*/\0,one/; s/^plugdev:.*/\0,one/; s/^power:.*/\0,one/; s/^netdev:.*/\0,one/; s/^scanner:.*/\0,one/; s/^users:.*/\0one/; s/^fuse:.*/\0one/; s/^wheel:.*/\0one/;' "$RDIR"/etc/group
+sed -i 's/^floppy:.*/\0one/; s/^audio:.*/\0one/; s/^video:.*/\0one/; s/^cdrom:.*/\0one/; s/^plugdev:.*/\0one/; s/^power:.*/\0one/; s/^netdev:.*/\0one/; s/^scanner:.*/\0one/; s/^users:.*/\0one/; s/^fuse:.*/\0one/; s/^wheel:.*/\0,one/;' "$RDIR"/etc/group
 mkdir -p "$RDIR"/home/one
 cp -r "$RDIR"/etc/skel/* "$RDIR"/etc/skel/.??* "$RDIR"/home/one/ 2>/dev/null
 
